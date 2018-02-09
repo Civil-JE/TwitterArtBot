241c1a598f0a8f0e6cff8844a6aa62bccd1041cd
Josh Eastman <joshua.e.eastman@gmail.com>
2018-02-09 18:52:01 -0500

    Added Twitter & Reorganized
    Reorganized & renamed some files/names for clarity.
    
    Added Twitter instance & can post to Twitter.

---
+++ instance.py
@@ -0,0 +1,55 @@
+import praw
+import tweepy
+
+class RedditInstance:
+    #Information is read from Credentials.txt
+    client_id = "" 
+    client_secret = ""
+    user_agent = ""
+    username = ""
+    password = ""
+    
+    reddit_instance = None
+
+    def __init__(self):
+        #credentialsFile = "credentials/Credentials.txt" #Prod
+        credentials_file = "credentials/testCredentials.txt" #Test
+        openedFile = open(credentials_file,"r")
+        credentials = openedFile.readlines()
+
+        #Had to add .strip to remove invisible whitespaces/leading characters
+        self.client_id = credentials[0].strip()
+        self.client_secret = credentials[1].strip()
+        self.user_agent = credentials[2].strip()
+        self.username = credentials[3].strip()
+        self.password = credentials[4].strip()
+
+
+        #Don't need to provide user details due to only needing a Read Only instance.
+        self.reddit_instance = praw.Reddit(client_id=self.client_id, client_secret=self.client_secret,
+                             user_agent=self.user_agent)  
+
+
+class TwitterInstance:
+    consumer_key = ""
+    consumer_secret = ""
+    access_token = ""
+    access_token_secret = ""
+
+    twitter_instance = None
+
+    def __init__(self):
+        #credentialsFile = "credentials/Credentials.txt" #Prod
+        credentials_file = "credentials/testCredentials.txt" #Test
+        openedFile = open(credentials_file,"r")
+        credentials = openedFile.readlines()
+
+        self.consumer_key = credentials[6].strip()
+        self.consumer_secret = credentials[7].strip()
+        self.access_token = credentials[8].strip()
+        self.access_token_secret = credentials[9].strip()
+
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+
+        self.twitter_instance = tweepy.API(auth)
--- main.py
+++ main.py
@@ -4,31 +4,28 @@ Updated: 02/08/2018
 Description: Main file for Twitter Art Bot
 '''
 import praw
-from createInstance import CreateRedditInstance
-
-x = CreateRedditInstance()
+import tweepy
+from instance import *
 
 GO = True
 
 
 
 while(GO):
-    x.setRedditInfo()
 
-    #Don't need to provide user details to only needing a Read Only account.
-    reddit = praw.Reddit(client_id=x.client_id, client_secret=x.client_secret, user_agent=x.user_agent)
+    
+    reddit = RedditInstance().reddit_instance
+ 
 
-    print(x.user_agent)
-    for submission in reddit.subreddit('art').top(time_filter='week',limit=2):
-        print(submission.title)
-        print(submission.url)
+    twitter = TwitterInstance().twitter_instance
+    
+    new_post = ""
 
+    print(reddit.read_only)
+    for submission in reddit.subreddit('art').top(time_filter='week',limit=1):
+        new_post = submission.title + " " +submission.url
 
-
-
-
-
-
+    twitter.update_status(new_post)
 
 
       
