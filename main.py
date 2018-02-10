adf54b97636888e5ebaec49684b44b6812628138
Josh Eastman <joshua.e.eastman@gmail.com>
2018-02-10 02:39:56 -0500

    Download Image and Post
    The basis for downloading an image based on a link and posting it to Twitter. Need to conditions to check where the site is from and what to use for the image location.

--- main.py
+++ main.py
@@ -3,30 +3,37 @@ Author: Josh Eastman
 Updated: 02/08/2018
 Description: Main file for Twitter Art Bot
 '''
+import os
 import praw
 import tweepy
+import urllib
 from instance import *
 
 GO = True
 
+reddit = RedditInstance().reddit_instance
+twitter = TwitterInstance().twitter_instance
 
 
-while(GO):
-
-    
-    reddit = RedditInstance().reddit_instance
- 
-
-    twitter = TwitterInstance().twitter_instance
-    
-    new_post = ""
+while(GO):    
+    new_post = ''
+    url = ''
 
     print(reddit.read_only)
-    for submission in reddit.subreddit('art').top(time_filter='week',limit=1):
-        new_post = submission.title + " " +submission.url
+    for submission in reddit.subreddit('pics').top(time_filter='week',limit=5):
+        url = submission.url
+        
+        new_post = submission.title + '' +submission.url
 
-    twitter.update_status(new_post)
+    image = url.replace('https://i.redd.it/','')
 
+    urllib.request.urlretrieve(url,image)
+    os.rename(image, "images/"+image)
 
       
     GO = False
+
+
+#    urllib.request.urlretrieve(url,"39XeDFj.jpg")
+#
+#    twitter.update_with_media("39XeDFj.jpg","test")
